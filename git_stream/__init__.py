63acaf703fae627f56cca8556d5d3fe15b191672
tardis4500 <tardis4500@users.noreply.github.com>
2026-02-10 21:05:58 +0000

    bump version 2.0.3 -> 2.0.4rc0 [skip ci]

--- git_stream/__init__.py
+++ git_stream/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'git-stream'
 __summary__ = 'Git Stream Implementation'
 __uri__ = 'https://github.com/arisilon/git-stream/'
 
-__version__ = '2.0.3'
+__version__ = '2.0.4rc0'
 __build_name__ = '{var:build_name}'
 __build_date__ = '{var:build_date}'
 
